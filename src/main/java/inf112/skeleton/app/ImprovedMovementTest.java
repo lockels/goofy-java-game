a1ce34117f0cf1e950cc9f6725fed0bbf42c087e
Jacob.Grahm-Haga <jagra1324@uib.no>
2024-02-28 19:46:13 +0100

    Improved Movement, added Input processing and adapting, added player with sprite and move info

--- src/main/java/inf112/skeleton/app/ImprovedMovementTest.java
+++ src/main/java/inf112/skeleton/app/ImprovedMovementTest.java
@@ -4,9 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import inf112.skeleton.app.myInput.MyInputAdapter;
 

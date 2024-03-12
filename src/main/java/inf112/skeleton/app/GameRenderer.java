b3f79f2b4be3a0b4fa0ec5d863efbbf80d22e47a
FredricHegland <fredric.hegland@gmail.com>
2024-03-12 15:45:08 +0100

    Added features

--- src/main/java/inf112/skeleton/app/GameRenderer.java
+++ src/main/java/inf112/skeleton/app/GameRenderer.java
@@ -95,12 +95,13 @@ public class GameRenderer extends ApplicationAdapter {
         }
 
         // Grid
-        grid = new Grid(10, 10, 800, 800);
+        grid = new Grid(8, 8, 800, 800);
         // Set entities for each cell in the grid
         grid.setEntity(1, 1, entities.get(0)); // For example, set player at (1, 1)
         grid.setEntity(2, 2, entities.get(1)); // For example, set enemy1 at (2, 2)
-        grid.setEntity(3, 3, entities.get(2)); // For example, set enemy2 at (3, 3)
+        grid.setEntity(4, 3, entities.get(2)); // For example, set enemy2 at (3, 3)
         grid.setEntity(4, 4, entities.get(3)); // For example, set enemy3 at (4, 4)
+        grid.setEntity(4, 5, entities.get(0));
 
         // Font
         font = new BitmapFont();

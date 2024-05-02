9cf6d1aea845818618d2ed6c33f60c4cc77814c0
@sara.dahle <stavangsara@gmail.com>
2024-05-02 22:22:57 +0100

    Added green and red button to Weapon Selection Screen

--- src/main/java/inf112/skeleton/app/view/WeaponSelectionScreen.java
+++ src/main/java/inf112/skeleton/app/view/WeaponSelectionScreen.java
@@ -38,7 +38,12 @@ public class WeaponSelectionScreen extends ScreenAdapter implements ApplicationL
     private Texture backButtonTexture; // Texture for the back to game button
     private Button backButton; 
     private Button treeButton;
+    private Button greenSwordButton;
+    private Button redSwordButton;
     private Texture treeButtonSwordTexture;
+    private Texture redButtonSwordTexture; 
+    private Texture greenButtonSwordTexture;
+    
 
      /**
         * Constructs a new WeaponSelectionScreen which sets up the environment to display
@@ -52,6 +57,8 @@ public class WeaponSelectionScreen extends ScreenAdapter implements ApplicationL
         this.backgroundTexture = new Texture(WEAPON_SELECTION);
         this.backButtonTexture = new Texture(BACK_TO_GAME_BUTTON); 
         this.treeButtonSwordTexture = new Texture(TREE_SWORD_BUTTON);
+        this.redButtonSwordTexture = new Texture(RED_SWORD_BUTTON);
+        this.greenButtonSwordTexture = new Texture(GREEN_SWORD_BUTTON);
     }
 
     
@@ -71,18 +78,6 @@ public class WeaponSelectionScreen extends ScreenAdapter implements ApplicationL
         float backButtonY = 20; 
         backButton.setPosition(backButtonX, backButtonY);
         stage.addActor(backButton);
-    
-        //Tree button
-        Drawable buttonDrawable1 = new TextureRegionDrawable(new TextureRegion(treeButtonSwordTexture));
-        Button.ButtonStyle buttonStyle1 = new Button.ButtonStyle();
-        buttonStyle1.up = buttonDrawable1; 
-        treeButton = new Button(buttonStyle1);
-        treeButton.setSize(280, 290);
-        // Position for the treeButton - positioned horizontally next to backButton
-        float treeButtonX = 110; 
-        float treeButtonY = 360; 
-        treeButton.setPosition(treeButtonX, treeButtonY);
-        stage.addActor(treeButton);
 
         backButton.addListener(new ClickListener() {
             @Override
@@ -103,6 +98,20 @@ public class WeaponSelectionScreen extends ScreenAdapter implements ApplicationL
             }
         });
 
+    
+        //Tree Sword button
+        Drawable buttonDrawable1 = new TextureRegionDrawable(new TextureRegion(treeButtonSwordTexture));
+        Button.ButtonStyle buttonStyle1 = new Button.ButtonStyle();
+        buttonStyle1.up = buttonDrawable1; 
+        treeButton = new Button(buttonStyle1);
+        treeButton.setSize(280, 290);
+        // Position for the treeButton - positioned horizontally next to backButton
+        float treeButtonX = 110; 
+        float treeButtonY = 355; 
+        treeButton.setPosition(treeButtonX, treeButtonY);
+        stage.addActor(treeButton);
+
+       
         treeButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -121,7 +130,74 @@ public class WeaponSelectionScreen extends ScreenAdapter implements ApplicationL
                 }
             }
         });
+
+
+        //Red Sword button
+        Drawable buttonDrawable2 = new TextureRegionDrawable(new TextureRegion(redButtonSwordTexture));
+        Button.ButtonStyle buttonStyle2 = new Button.ButtonStyle();
+        buttonStyle2.up = buttonDrawable2; 
+        redSwordButton = new Button(buttonStyle2);
+        redSwordButton.setSize(280, 290);
+        float redSwordButtonX = 430; 
+        float redSwordButtonY = 355; 
+        redSwordButton.setPosition(treeButtonX, treeButtonY);
+        stage.addActor(redSwordButton);
+        redSwordButton.setPosition(redSwordButtonX, redSwordButtonY);
+        stage.addActor(redSwordButton);
+        
+        redSwordButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                try {
+                    SpriteBatch batch = game.getBatch(); 
+                    GameLogic gameLogic = game.getGameLogic();
+                    OrthographicCamera cam = game.getCamera(); 
+
+                    if (gameLogic != null && cam != null) {
+                        game.setScreen(new GameActiveScreen(game, gameLogic, batch, cam));
+                    } else {
+                        System.out.println("Game logic or camera is null");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //Green Sword Button
+        Drawable buttonDrawable3 = new TextureRegionDrawable(new TextureRegion(greenButtonSwordTexture));
+        Button.ButtonStyle buttonStyle3 = new Button.ButtonStyle();
+        buttonStyle3.up = buttonDrawable3; 
+        greenSwordButton = new Button(buttonStyle3);
+        greenSwordButton.setSize(280, 290);
+        float greenSwordButtonX = 270; 
+        float greenSwordButtonY = 60; 
+        greenSwordButton.setPosition(greenSwordButtonX, greenSwordButtonY);
+        stage.addActor(greenSwordButton);
+
+        greenSwordButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                try {
+                    SpriteBatch batch = game.getBatch(); 
+                    GameLogic gameLogic = game.getGameLogic();
+                    OrthographicCamera cam = game.getCamera(); 
+
+                    if (gameLogic != null && cam != null) {
+                        game.setScreen(new GameActiveScreen(game, gameLogic, batch, cam));
+                    } else {
+                        System.out.println("Game logic or camera is null");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
+
+
+
 
     @Override
     public void render(float delta) {
@@ -141,7 +217,9 @@ public class WeaponSelectionScreen extends ScreenAdapter implements ApplicationL
         batch.dispose();
         backgroundTexture.dispose();
         backButtonTexture.dispose();
-        treeButtonSwordTexture.dispose();;
+        treeButtonSwordTexture.dispose();
+        redButtonSwordTexture.dispose();
+        greenButtonSwordTexture.dispose();
         stage.dispose();
     }
 
